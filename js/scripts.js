bfbf00c53273b201ca1a642679d72e8c778009f6
Tyler Bowerman <tylerbowerman@yahoo.com>
2020-04-10 14:29:52 -0700

    debug js file.

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,7 @@
 function PizzaToBuy () {
   this.pizzas = [];
   this.currentId = 0;
+  this.price = 0;
   console.log(this.pizzas);
 }
 
@@ -10,11 +11,12 @@ PizzaToBuy.prototype.addPizza = function (pizza) {
   this.pizzas.push(pizza);
 }
 
-function Pizza(size,cheese, meat,veggies) {
+function Pizza(size,cheese, meat,veggies,price) {
   this.size = size;
   this.cheese = cheese;
   this.meat = meat;
   this.veggies = veggies;
+  this.price = price;
 }
 
 PizzaToBuy.prototype.assignId = function() {
@@ -74,6 +76,10 @@ $(document).ready(function() {
     var inputtedCheese = $("#cheese").val();
     var inputtedMeat = $("#meat").val();
     var inputtedVeggies = $("#veggies").val();
+
+    $("#startOrder").click(function() {
+      $("#pizzaOrder").show();
+    });
 
     var newPizza = new Pizza(inputtedSize,inputtedCheese,inputtedMeat,inputtedVeggies);
     pizzaToBuy.addPizza(newPizza);
