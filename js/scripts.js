db6d390753ac8f19a0b8d33335f55f0d2857c7bb
Tyler Bowerman <tylerbowerman@yahoo.com>
2020-04-10 14:46:42 -0700

    debug js file.

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@
 function PizzaToBuy () {
   this.pizzas = [];
   this.currentId = 0;
-  this.price = 0;
+  
   console.log(this.pizzas);
 }
 
@@ -11,12 +11,11 @@ PizzaToBuy.prototype.addPizza = function (pizza) {
   this.pizzas.push(pizza);
 }
 
-function Pizza(size,cheese, meat,veggies,price) {
+function Pizza(size,cheese, meat,veggies) {
   this.size = size;
   this.cheese = cheese;
   this.meat = meat;
   this.veggies = veggies;
-  this.price = price;
 }
 
 PizzaToBuy.prototype.assignId = function() {
@@ -70,6 +69,11 @@ function showPizza(pizza) {
 $(document).ready(function() {
   var pizzaToBuy = new PizzaToBuy();
 
+  $("button#startOrder").click(function () {
+    $("#pizzaOrder").show();
+    $("#startOrder").hide();
+  });
+
   $("#pizzaOrder").submit(function(event) {
     event.preventDefault();
     var inputtedSize = $("#size").val();
@@ -77,9 +81,7 @@ $(document).ready(function() {
     var inputtedMeat = $("#meat").val();
     var inputtedVeggies = $("#veggies").val();
 
-    $("#startOrder").click(function() {
-      $("#pizzaOrder").show();
-    });
+    
 
     var newPizza = new Pizza(inputtedSize,inputtedCheese,inputtedMeat,inputtedVeggies);
     pizzaToBuy.addPizza(newPizza);
