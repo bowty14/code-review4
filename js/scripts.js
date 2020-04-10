0af9010434e93681d64f34cf63ac3be37ea33c0c
Tyler Bowerman <tylerbowerman@yahoo.com>
2020-04-10 08:29:36 -0700

    add a add pizza prototype.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,11 @@
 function PizzaToBuy () {
-  this.pizza = [];
+  this.pizzas = [];
   this.currentId = 0;
   console.log("PizzaToBuy", PizzaToBuy);
 }
+
+PizzaToBuy.prototype.addPizza = function(pizza) {
+  pizza.id = this.assignId();
+  this.pizzas.push(pizza);
+}
+
