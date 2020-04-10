bf3c57fc05ef71acf12d386c364e0f13631b5aa0
Tyler Bowerman <tylerbowerman@yahoo.com>
2020-04-10 08:43:28 -0700

    add a prototype that will find pizzas stored in an array based off the assigned id. add a price formula based off size of pizza.

--- js/scripts.js
+++ js/scripts.js
@@ -9,9 +9,31 @@ PizzaToBuy.prototype.addPizza = function(pizza) {
   this.pizzas.push(pizza);
 }
 
-function Pizza(small, medium,large,XL) {
-  this.small = small;
-  this.medium = medium;
-  this.large = large;
-  this.XL = XL;
+function Pizza(size) {
+  this.size = size;
+}
+
+PizzaToBuy.prototype.findPizza = function (id) {
+  for (var i = 0; i < this.pizzas.length; i++) {
+    if (this.pizzas[i]) {
+      if (this.pizzas[i].id === id) {
+        return this.pizzas[i];
+      }
+    }
+  };
+  return false;
+}
+
+Pizza.prototype.price = function () {
+  var price = 15;
+  if (this.size === "small") {
+    price *= .9;
+  } else if (this.size === "medium") {
+    price *= 1.2;
+  } else if (this.size === "large") {
+    price *= 2;
+  } else {
+    price *= 2.5;
+  }
+  return price;
 }
